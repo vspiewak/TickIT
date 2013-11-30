cf667d1346633f916363ae77ac4c52f359f819f0
Vincent Spiewak <vspiewak@gmail.com>
2013-11-30 21:39:59 +0100

    Add persistence in counter.file

--- server.js
+++ server.js
@@ -1,26 +1,176 @@
+#!/bin/env node
+//  OpenShift sample Node application
 var express = require('express');
-var app = express();
+var fs      = require('fs');
 
-var code = 0;
+/**
+ *  Define the sample application.
+ */
+var TickItApp = function() {
 
-function leftPad(val) {
-  var ret = "" + val;
-  var pad = "000";
-  return pad.substring(0, pad.length - ret.length) + ret;
-}
+    //  Scope.
+    var self = this;
 
-app.use("/", express.static(__dirname + '/public'));
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
 
-app.get('/api/code', function(req, res) {
-  code++;
-  var ret = leftPad(code);
-  res.json({ code: ret });
-});
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.get('/api/reset', function(req, res) {
-  code = 0;
-  res.send(200);
-});
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
 
-app.listen(3000);
+
+    /**
+     *  Populate the cache.
+     */
+    self.populateCache = function() {
+        if (typeof self.zcache === "undefined") {
+            self.zcache = { 'index.html': '' };
+        }
+
+        //  Local cache for static content.
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    };
+
+
+    /**
+     *  Retrieve entry (content) from cache.
+     *  @param {string} key  Key identifying content to retrieve from cache.
+     */
+    self.cache_get = function(key) { return self.zcache[key]; };
+
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating TickIt app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+    
+    /**
+     * pad to left 
+     */
+    self.leftPad = function(val) {
+      var ret = "" + val;
+      var pad = "000";
+      return pad.substring(0, pad.length - ret.length) + ret;
+    }
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function() {
+        self.routes = { };
+
+        /*self.routes['/'] = function(req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(self.cache_get('index.html') );
+        };*/
+
+        self.routes['/api/code'] = function(req, res) {
+            
+            var counter = fs.readFileSync("counter.txt").toString();
+            counter = parseInt(counter);
+            counter++;
+            fs.writeFile("counter.txt", counter);
+
+            var ret = self.leftPad(counter);
+            res.json({ code: ret });
+        };
+
+        self.routes['/api/reset'] = function(req, res) {
+            fs.writeFile("counter.txt", 0);
+            res.send(200);
+        };
+
+    }
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express();
+        self.app.use("/", express.static(__dirname + '/public'));
+
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
+
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        //self.populateCache();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};   /*  TickIt Application.  */
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new TickItApp();
+zapp.initialize();
+zapp.start();
 
